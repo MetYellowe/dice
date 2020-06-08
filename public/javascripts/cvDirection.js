a5658602a2fd660f7d0f4eebdd6abe6f95ae8278
MetYellowe <yellowee94@gmail.com>
2020-06-08 20:15:33 +0300

    update cv

--- public/javascripts/cvDirection.js
+++ public/javascripts/cvDirection.js
@@ -20,3 +20,12 @@ var listener1 = function(e) {
 }
 
 document.addEventListener('click', listener1);
+
+var windowHeight = document.documentElement.clientHeight;
+var windowWidth = document.documentElement.clientWidth;
+window.onload = function() {
+    var divs = document.querySelectorAll('div');
+    for(var key of divs) {
+        key.style.left = windowWidth / 2 - key.clientWidth / 2 + 'px';
+    }
+};
